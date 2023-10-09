eef2492224f1f89b363ec9e6381962fbff2e5753
Joe <joachimgerard66@gmail.com>
2023-10-09 22:02:11 +0800

    Added in modals for transactions and transfer

--- src/Account.jsx
+++ src/Account.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect  } from "react";
 import { toast } from 'react-toastify';
 import {Helmet} from "react-helmet";
 import Footer from './Footer';
+import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import userData from './users.json';
 import { useUser } from "./UserContext";
@@ -9,15 +10,28 @@ import Navbar from './Navbar';
 import 'bootstrap/dist/css/bootstrap.css';
 import "./App.scss";
 
-
 function Account() {
   const { user } = useUser();
   // Assuming you have the user's email after login
-  const [balance, setBalance] = useState(user ? user.balance : 0);
+  const [balance, setBalance] = useState(user ? parseInt(user.balance) : 0);
   const [incrementBy, setIncrementBy] = useState(1);
   const [decrementBy, setDecrementBy] = useState(1);
+  const [showInc, setShowInc] = useState(false);
+  const [showDec, setShowDec] = useState(false);
+  const [showTran, setShowTran] = useState(false);
+  const [message, setMessage] = useState('');
+ 
+  const handleChange = event => {
+    setMessage(event.target.value);
+
+    console.log('value is:', event.target.value);
+  };
+  const handleCloseInc = () => setShowInc(false);
+  const handleCloseDec = () => setShowDec(false);
+  const handleCloseTran = () => setShowTran(false);
 
 const increment = () => {
+  setShowInc(true);
   setBalance(balance + parseInt(incrementBy));
 };
 
@@ -25,11 +39,26 @@ const increment = () => {
 const decrement = () => {
   // Add a check to prevent count from going below 0
   if (balance > parseInt(decrementBy) && balance !== 20) {
+    setShowDec(true);
     setBalance(balance - parseInt(decrementBy));
   } else {
     toast("Minimum account balance of RM20 is required for transactions to be made, please withdraw an appropriate amount or deposit enough cash to continue withdrawal activity");
   }
 };
+
+
+
+const transfer = () => {
+  // Add a check to prevent count from going below 0
+  if (balance > parseInt(decrementBy) && balance !== 20) {
+    setShowTran(true);
+    setBalance(balance - parseInt(decrementBy));
+  } else {
+    toast("Minimum account balance of RM20 is required for transfers to be made, please transfer an appropriate amount or deposit enough cash to continue withdrawal activity");
+  }
+};
+
+
     return(
       <div class ="account">
         <Helmet>
@@ -67,17 +96,76 @@ const decrement = () => {
         <input type="number" class="numberInput" value={incrementBy} onChange={(e) => setIncrementBy(e.target.value)}/>
         <Button variant="info"onClick={increment}>Increment</Button>
       </div>
-
+      <Modal show={showInc} onHide={handleCloseInc}>
+        <Modal.Header closeButton>
+          <Modal.Title>Bank XYZ</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>You have deposited RM{incrementBy} into your account</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseInc}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
       <div>
         <label>Withdraw: </label>
         <input type="number" class="numberInput" value={decrementBy} onChange={(e) => setDecrementBy(e.target.value)}/>
         <Button variant="info" onClick={decrement}>Decrement</Button>
       </div>
+      <Modal show={showDec} onHide={handleCloseDec}>
+        <Modal.Header closeButton>
+          <Modal.Title>Bank XYZ</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>You have withdrawn RM{decrementBy} from your account</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseDec}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
                 </div>
             </div>
+        </section>
+
+        <section class="border bottom" id="features">
+        <div class="container px-5 my-5 px-5">
+                <div class="text-center mb-2">
+                    <h2 class="fw-bolder">Transfer</h2>
+                </div>
+                <div class="container px-5 my-5 px-5">
+                <div class="text-center">
+      <div>
+      <label>Transfer To: </label>
+      <input type="text" id="message" class="textInput" name="message" onChange={handleChange} value={message}/>
+       </div>
+       <div>
+        <label>Transfer: </label>
+        <input type="number" class="numberInput" value={decrementBy} onChange={(e) => setDecrementBy(e.target.value)}/>
+        <Button variant="info" onClick={transfer}>Transfer</Button>
+      </div>
+      <Modal show={showTran} onHide={handleCloseTran}>
+        <Modal.Header closeButton>
+          <Modal.Title>Bank XYZ</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>You have transferred RM{decrementBy} from your account to {message}.</Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseTran}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
+                </div>
+            </div>
+            </div>
+        </section>
+
+        <section class="py-3 border-bottom">
+
         </section>
        <Footer /> 
     </div> 
   );
 }
+
+
 export default Account;
