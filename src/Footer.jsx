9c656c62f22beb581d998a205af951bb5b4de107
Joe <joachimgerard66@gmail.com>
2023-10-25 21:49:22 +0800

    Updated appearance of footer, header is next.

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -2,9 +2,22 @@ import React from 'react';
 import "./App.scss";
 function Footer() {
   return (
-    <footer class="pt-1 pb-1">
-      &copy; @JoeG67 copyright. 2023 XYZ Bank. All rights reserved.
-    </footer>
+<footer class="pt-1 pb-1">
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        © 2023 JoeG67. All rights reserved.
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-12 text-center">
+        Unauthorized use or reproduction of this application, its content, or design is prohibited.
+        For inquiries and permissions, please contact us at my GitHub.
+      </div>
+    </div>
+  </div>
+</footer>
+
   );
 };
 export default Footer;
