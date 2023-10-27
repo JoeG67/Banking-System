e029fd481b1a0bd881a42b2740f2b5aed2241ed4
Joe <joachimgerard66@gmail.com>
2023-10-27 20:49:52 +0800

    Minor updates to navbar code

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -5,15 +5,15 @@ import "./App.scss";
 
 function Navbar() {
   return (
-    <nav class="navbar navbar-expand-lg  mb-1 rounded">
+    <nav class="navbar navbar-expand-lg mb-1 rounded">
       <div class="container-fluid">
         <li><Link to="/home">
-          <img src="/docs/4.0/assets/brand/bootstrap-solid.svg" width="30" height="30" class="d-inline-block align-top" alt="" /><a class="navbar-brand" href="/#">XYZBank</a></Link></li>
+          <a class="navbar-brand" href="/#">XYZBank</a></Link></li>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul class="navbar-nav me-auto custom-center-links">
+          <ul class="navbar-nav me-auto">
             <li>
               <Link to="/home">Home</Link>
             </li>
@@ -26,7 +26,7 @@ function Navbar() {
             <li>
             </li>
           </ul>
-          <form class="" role="search">
+          <form class="custom-center-links" role="search">
             <Link to="/"><Button variant="info" class="btn btn-info text-white fw-bold">Logout</Button></Link>
           </form>
         </div>
