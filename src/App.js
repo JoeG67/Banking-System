03cb0d8aa9caacee2beb4178db7313a22d6cc0a5
Joe <joachimgerard66@gmail.com>
2023-11-06 20:50:15 +0800

    Added additional comments into App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes, BrowserRouter } from 'react-router-dom';  //React Router library is used for handling client-side routing
 import "./App.scss";
-import Login from './Login'; // Assuming this is the name of your login form component
+import Login from './Login'; 
 import Home from './Home'
 import Account from './Account'
 import Contact from './Contact'
@@ -9,8 +9,10 @@ import Contact from './Contact'
 function App() {
 
   return (
-    <BrowserRouter>
-      <Routes>
+
+    <BrowserRouter>  {/*BrowserRouter provides the routing infrastructure for your application */}
+      <Routes>  {/*Router component used to define the routing configuration for your application */}
+       {/* Route is used to define individual routes within the <Routes> component, specifies the path (URL) for which the route should be active*/}
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
         <Route path="/account" element={<Account />} />
