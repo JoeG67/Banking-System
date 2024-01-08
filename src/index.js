e993813215ec299c8e2e5c619f0d96ed11e64bec
Joe <joachimgerard66@gmail.com>
2024-01-08 22:37:26 +0800

    Removed redundant code

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { UserProvider } from './UserContext';
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <UserProvider>
